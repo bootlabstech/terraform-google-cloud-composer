f2662c0b29dd77250a22ea252975405637911b5b
Snekhapugal <SnekhaPuhazendhi>
2025-04-11 19:59:48 +0530

    enable_ip_masq_agent variablised

--- main.tf
+++ main.tf
@@ -11,14 +11,9 @@ resource "google_composer_environment" "composer" {
   }
   config {
     environment_size = var.environment_size
-    software_config {
-      image_version = var.image_version
-
-      # Only set these values if enable_software_config is true
-      airflow_config_overrides = var.enable_software_config ? var.airflow_config_overrides : null
-      pypi_packages            = var.enable_software_config ? var.pypi_packages : null
-      env_variables            = var.enable_software_config ? var.env_variables : null
-    }
+    # software_config {
+    #   image_version = var.image_version
+    # }
     node_config {
       network              = var.network
       subnetwork           = var.subnetwork
@@ -35,15 +30,16 @@ resource "google_composer_environment" "composer" {
     encryption_config {
       kms_key_name = var.kms_key_name
     }
-    # dynamic "software_config" {
-    #   for_each = var.enable_software_config ? [{}] : []
-    #   content {
-    #     airflow_config_overrides = var.airflow_config_overrides
-    #     pypi_packages = var.pypi_packages
-    #     env_variables = var.env_variables
-    #     
-    #   }
-    # }
+    dynamic "software_config" {
+      for_each = var.enable_software_config ? [{}] : []
+      content {
+        image_version = var.image_version
+        airflow_config_overrides = var.airflow_config_overrides
+        pypi_packages = var.pypi_packages
+        env_variables = var.env_variables
+        
+      }
+    }
     dynamic "private_environment_config" {
       for_each = var.use_private_environment ? [{}] : []
       content {
